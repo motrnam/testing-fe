c474af7ae9fcfa46bd402c3186a6a1aa5d866201
motrnam <motrnam@gmail.com>
2023-01-10 11:01:20 +0330

    salam sobh

---
+++ additional1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int ar[4000];
+    for(int i=1;i<1500;i++){
+        if(i<10){
+            ar[i-1]=i;
+        }
+        if((i<9)&&(100>i)){
+
+        }
+    }
+
+}
